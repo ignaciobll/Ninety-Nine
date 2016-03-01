e1204b87ec3f5d22895ffdf9328063df3caa4c0e
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2016-03-02 00:04:43 +0100

    Test import. Try of 7

--- Haskell/Problems1-10.hs
+++ Haskell/Problems1-10.hs
@@ -1,32 +1,42 @@
+import Data.List
+import Test.QuickCheck
+
+-- Problem 1 --
 myLast :: [a] -> a
 myLast [] = error "No last item of an empty list"
 myLast [x] = x
 myLast (_:xs) = myLast xs
 
+-- Problem 2 --
 myButLast :: [a] -> a
 myButLast [] = error "No last but one element"
 myButLast [x] = error "No last but one element"
 myButLast [x,_] = x
 myButLast (_:xs) = myButLast xs
 
+-- Problem 3 --
 myTake :: Int -> [a] -> [a]
+myTake 0 _ = []
 myTake z [] = []
 myTake z [x] = x:[]
 myTake z (x:xs) = if z > 1 then x:(myTake (z-1) xs) else x:[] 
                    
 elementAt :: Int -> [a] -> a
 elementAt z (xs) = myLast (myTake z (xs))
-                   
+
+-- Problem 4 --                   
 myLength :: [a] -> Int
 myLength [] = 0
 myLength [x] = 1
 myLength (_:xs) = 1 + myLength (xs)
 
+-- Problem 5 --
 myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse [x] = [x]
 myReverse (x:xs) = myReverse xs ++ [x]
 
+-- Problem 6 --
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome [] = False
 isPalindrome [x] = True
@@ -34,3 +44,20 @@ isPalindrome [x,y] = x == y
 isPalindrome (x:xs) = x == (last xs) && isPalindrome (init xs)
 
 -- Problema 7 --
+{-data NestedList a = Elem a | List [NestedList a]
+my_Flatten :: NestedList a -> [a]
+my_Flatten (Elem a) = [a]
+my_Flatten (List []) = []
+my_Flatten (x:xs) = (my_Flatten x) ++ (my_Flatten xs)
+ -}
+
+-- Problem 8 --
+
+-- Problem 9 --
+
+
+-------------  Test --------------
+
+
+prop_myTake :: Int -> [a] -> Bool
+prop_myTake z xs = (length (myTake z xs)) == (length (take z xs)) --Not working
